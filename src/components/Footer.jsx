98661611c66e873ab8fa08c4a3c7de961ee29e4d
ashfaque910 <ashfaque910ahmad@gmail.com>
2024-05-21 17:47:33 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer
+        style={{backgroundColor: "#850F8D", color: "#387476"}}
+        className="footer-content text-light py-3"
+      >
+        <div className="container text-center">
+          <p>&copy; 2024 Medical Clinic. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
